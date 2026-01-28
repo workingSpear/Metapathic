3d001f8decda86ed5398a86542b374bbff34fbfb
Gareg <reg_ga@farmorereal.com>
2026-01-28 15:03:16 -0800

    Added a healthbar scene and added health bar to enemy scene
    Also added basic gdscript implementation of a take_damage() function which updates the healthbar

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -1,1 +1,18 @@
 extends Node3D
+
+@export var max_health: float = 10
+
+@onready var current_health: float = max_health
+
+func take_damage(damage):
+	current_health -= damage
+	
+	# update healthbar
+	$"Healthbar Viewport/Healthbar".value = current_health / max_health
+	
+	if (current_health <= 0):
+		current_health = 0
+		die()
+
+func die():
+	return
