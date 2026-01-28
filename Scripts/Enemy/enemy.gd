2c391366ec07a50dd42e5bfc185411cdc358af40
Gareg <reg_ga@farmorereal.com>
2026-01-28 15:11:11 -0800

    updated enemy script to be of new Enemy class and made it extend rigidbody3D as that is the current node type of enemy

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -1,18 +1,24 @@
-extends Node3D
+class_name Enemy
+extends RigidBody3D
+
+# A basic dummy enemy base class which can take damage and die.
 
 @export var max_health: float = 10
 
 @onready var current_health: float = max_health
 
-func take_damage(damage):
+func _ready() -> void:
+	pass
+
+func take_damage(damage) -> void:
 	current_health -= damage
 	
-	# update healthbar
+	# update healthbar value
 	$"Healthbar Viewport/Healthbar".value = current_health / max_health
 	
 	if (current_health <= 0):
 		current_health = 0
 		die()
 
-func die():
-	return
+func die() -> void:
+	pass
